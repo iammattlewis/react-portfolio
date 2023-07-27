2f8c89e71bb50c11ea14ed4f719cca9bc92e3dee
Matthew Lewis <matthewlewis@Matthews-MBP.lan>
2023-07-27 17:56:57 -0400

    added experience icon s & updated their layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
 import 'primereact/resources/themes/arya-purple/theme.css';
 import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css'
-import { classNames } from "primereact/utils";
+import 'primeicons/primeicons.css';
 import { Image } from "primereact/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCat } from "@fortawesome/free-solid-svg-icons";
 import { faBaseball } from "@fortawesome/free-solid-svg-icons";
 import { faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import { faApple, faBootstrap, faCss3Alt, faHtml5, faNodeJs, faReact, faSquareJs, faWindows } from "@fortawesome/free-brands-svg-icons";
 import scrollToSection from "./ScrollToSection";
 import scrollToTop from "./ScrollToTop";
 import FrozenMenuBar from "./FrozenMenuBar";
-
-
-
 
 function App() {
   const items = [
@@ -43,15 +40,68 @@ function App() {
       icon: "pi pi-user-plus",
       command: () => scrollToSection("connect", 80),
     }
-  ]
-
-
+  ];
 
   return (
     <div className="container">
       <FrozenMenuBar items={items}/>
       <h1>Matthew Lewis - Software Developer</h1>
-      <h2 id="projects">Check Out My Projects:</h2>
+      <h3>Experienced With:</h3>
+      <div className="experienced-icons">
+        <div className="row">
+          <div className="icon-container">
+            <div className="icon">
+              <FontAwesomeIcon icon={faReact} style={{ fontSize: "1.75rem" }}/>
+            </div>
+            <p>React</p>
+          </div>
+          <div className="icon-container">
+            <div className="icon">
+              <FontAwesomeIcon icon={faSquareJs} style={{ fontSize: "1.75rem" }}/>
+            </div>
+            <p>JavaScript</p>
+          </div>
+          <div className="icon-container">
+            <div className="icon">
+              <FontAwesomeIcon icon={faHtml5} style={{ fontSize: "1.75rem" }}/>
+            </div>
+            <p>HTML</p>
+          </div>
+          <div className="icon-container">
+            <div className="icon">
+              <FontAwesomeIcon icon={faCss3Alt} style={{ fontSize: "1.75rem" }}/>
+            </div>
+            <p>CSS</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="icon-container">
+            <div className="icon">
+              <FontAwesomeIcon icon={faBootstrap} style={{ fontSize: "1.75rem" }} />
+            </div>
+            <p>Bootstrap</p>
+          </div>
+          <div className="icon-container">
+            <div className="icon">
+              <FontAwesomeIcon icon={faNodeJs} style={{ fontSize: "1.75rem" }} />
+            </div>
+            <p>Node.js</p>
+          </div>
+          <div className="icon-container">
+            <div className="icon">
+              <FontAwesomeIcon icon={faApple} style={{ fontSize: "1.75rem" }} />
+            </div>
+            <p>macOS</p>
+          </div>
+          <div className="icon-container">
+            <div className="icon">
+              <FontAwesomeIcon icon={faWindows} style={{ fontSize: "1.75rem" }} />
+            </div>
+            <p>Windows</p>
+          </div>
+        </div>
+      </div>
+      <h3 id="projects">Check Out My Projects:</h3>
       <div className="text-center">
         <img src="/decoder_screenshot.jpg" width="500dvh"></img>
         <div className="links">
@@ -61,14 +111,12 @@ function App() {
       </div>
       <h2 id="about">About Me:</h2>
       <p className="centered-text">
-      After years of working in warehouses,<br />
-      I finally decided it was time to pursue an interest I've always had: software.<br />
-      I'm looking forward to jumping into the development process<br />
-      and being part of a creative team solving real-world problems for myself and others.<br />
+        After years of working in warehouses,<br />
+        I finally decided it was time to pursue an interest I've always had: software.<br />
+        I'm looking forward to jumping into the development process<br />
+        and being part of a creative team solving real-world problems for myself and others.<br />
       </p>
-      <h3>
-        When I'm not learning about software, I enjoy:
-      </h3>
+      <h3>When I'm not learning about software, I enjoy:</h3>
       <div className="hobbies">
         <FontAwesomeIcon icon={faCat} />
         <h5>Spending time with my cat, Newman.</h5>
@@ -84,7 +132,6 @@ function App() {
         <h5>Spending time with my friends.</h5>
         <FontAwesomeIcon icon={faUserGroup} flip="horizontal" />
       </div>
-
       <Image src="/Newman.jpg" alt="Newman" width="500" />
       <div>
         <h2 id="experience">My Resumé:</h2>
